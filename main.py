6a122c273a849391c2dbc3935a93a9f452c449dc
Leonardo Lima <leonardolimastos2003@gmail.com>
2025-02-17 15:58:46 -0300

    filtrando afiliações e nomes de personagens

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from deep_translator import GoogleTranslator
 API_BASE_URL = 'https://last-airbender-api.fly.dev/'
 
 class DadosPersonagens:
@@ -6,9 +7,34 @@ class DadosPersonagens:
     url_api_characters = f'{API_BASE_URL}api/v1/characters'
     
     def __init__(self):
-        self.data = self.totalData()
+        self.data = self.pushData()
+        self.translator = GoogleTranslator(source='en', target='pt')
 
-    def totalData(self):
+
+    def pushData(self):
         response = requests.get('https://last-airbender-api.fly.dev/api/v1/characters')
         data = response.json()
         return data
+    
+    names = []
+    def filterNames(self):
+        for chacacter in self.data:
+            try:
+                self.names.append(chacacter['name'])
+            except KeyError:
+                self.names.append(chacacter['Without Name'])
+                
+    affiliations = []    
+    def filterAffiliation(self):
+        for character in self.data:
+            try:
+                self.affiliations.append(character["affiliation"])
+            except KeyError:    
+                self.affiliations.append('Without affiliations')
+
+    def translateData(self):
+        ...
+
+chacacters = DadosPersonagens()
+chacacters.filterAffiliation()
+print(chacacters.affiliations)
